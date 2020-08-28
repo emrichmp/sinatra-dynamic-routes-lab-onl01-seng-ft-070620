45072fc607a22cb5c854ca45c96a924112bead46
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-08-28 19:11:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,6 +28,9 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    
+    @op = params[:operation]
+    @n1 = params[:number1].to_i
+    @n2 = params[:number2].to_i
+    erb :operation
   end
 end
