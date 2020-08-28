62aa9dfe42424eea844a74e022336163cd704d3c
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-08-28 19:08:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,12 +17,12 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    # @1 = params[:word1]
-    # @2 = params[:word2]
-    # @3 = params[:word3]
-    # @4 = params[:word4]
-    # @5 = params[:word5]
-    # erb :saywords
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    erb :saywords
   end
   
   get '/:operation/:number1/:number2' do
